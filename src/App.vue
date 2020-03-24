e4c6abc022cabdf61e49dad29c608f1fb24e66be
Jakob Niggel <jakobniggel@gmail.com>
2020-03-24 18:11:13 +0100

    deploy

--- src/App.vue
+++ src/App.vue
@@ -64,16 +64,142 @@ export default {
       retailer: {},
       map: [
         {
-          featureType: "administrative",
+          featureType: "administrative.country",
           elementType: "geometry",
           stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "administrative.land_parcel",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "administrative.locality",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "administrative.neighborhood",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "administrative.province",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "landscape.man_made",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#EEEDEB"
+            }
+          ]
+        },
+        {
+          featureType: "landscape.natural",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#C9D7CB"
+            },
+            {
+              saturation: -20
+            },
+            {
+              lightness: 40
+            },
+            {
+              visibility: "on"
+            },
+            {
+              weight: 1.5
+            }
+          ]
+        },
+        {
+          featureType: "landscape.natural.landcover",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "landscape.natural.terrain",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "poi.attraction",
+          elementType: "labels",
+          stylers: [
+            {
+              color: "#C9D7CB"
+            },
             {
               visibility: "off"
             }
           ]
         },
         {
-          featureType: "administrative.land_parcel",
+          featureType: "poi.business",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#C9D7CB"
+            }
+          ]
+        },
+        {
+          featureType: "poi.business",
+          elementType: "labels",
+          stylers: [
+            {
+              color: "#C9D7CB"
+            },
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "poi.government",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#C9D7CB"
+            }
+          ]
+        },
+        {
+          featureType: "poi.government",
           elementType: "labels",
           stylers: [
             {
@@ -82,7 +208,17 @@ export default {
           ]
         },
         {
-          featureType: "poi",
+          featureType: "poi.medical",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#E1C4C6"
+            }
+          ]
+        },
+        {
+          featureType: "poi.medical",
+          elementType: "labels",
           stylers: [
             {
               visibility: "off"
@@ -90,7 +226,130 @@ export default {
           ]
         },
         {
-          featureType: "poi",
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "poi.place_of_worship",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "poi.place_of_worship",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "poi.school",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "poi.school",
+          elementType: "labels",
+          stylers: [
+            {
+              color: "#AECDBF"
+            },
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "poi.sports_complex",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#AECDBF"
+            }
+          ]
+        },
+        {
+          featureType: "poi.sports_complex",
+          elementType: "labels",
+          stylers: [
+            {
+              color: "#AECDBF"
+            },
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#E9D5BC"
+            },
+            {
+              saturation: 5
+            },
+            {
+              lightness: 5
+            },
+            {
+              visibility: "on"
+            }
+          ]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "road.highway",
           elementType: "labels.text",
           stylers: [
             {
@@ -99,8 +358,8 @@ export default {
           ]
         },
         {
-          featureType: "road",
-          elementType: "labels.icon",
+          featureType: "road.highway.controlled_access",
+          elementType: "labels",
           stylers: [
             {
               visibility: "off"
@@ -112,15 +371,25 @@ export default {
           elementType: "labels",
           stylers: [
             {
-              visibility: "off"
+              visibility: "on"
             }
           ]
         },
         {
           featureType: "transit",
+          elementType: "labels.icon",
           stylers: [
             {
               visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#A4BCBF"
             }
           ]
         }
